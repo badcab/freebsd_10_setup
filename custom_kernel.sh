8ea11cafeb4a001155bfb8e2612f97ead8e78859
Michael Spear <github@bsd4breakfast.us>
2014-02-21 10:24:00 -0600

    Update custom_kernel.sh

--- custom_kernel.sh
+++ custom_kernel.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+SVN_URL=svn://svn.freebsd.org/base/releng/$(freebsd-version | cut -d - -f 1)
+
+svn_update(){
+  #if svn is not installed then install
+  # pkg install devel/subversion
+  if [ ! -d /usr/src/.svn ] ; then
+    svn co' $SVN_URL '/usr/src
+  fi
+  svn update /usr/src
+}
+
